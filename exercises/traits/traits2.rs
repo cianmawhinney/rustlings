8f5f27bedb3d90cb1619a1c5226c2caf8e3ed825
Cian Mawhinney <cianmawhinney@gmail.com>
2023-09-10 16:37:52 +0100

    [exercise] traits

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -8,13 +8,23 @@
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
-// TODO: Implement trait `AppendBar` for a vector of strings.
+impl AppendBar for Vec<String> {
+
+    // wrong end of the stick...
+    // fn append_bar(self) -> Vec<String> {
+    //     self.into_iter().map(|x| format!("{}Bar", x)).collect()
+    // }
+
+    fn append_bar(mut self) -> Self {
+        self.push("Bar".to_string());
+        
+        return self
+    }
+}
 
 #[cfg(test)]
 mod tests {
